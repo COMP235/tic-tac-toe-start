c353195d361d6e897345ab980d8bed1b745a1440
profjeff <46285923+profjeff@users.noreply.github.com>
2022-01-30 09:55:00 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Tictactoe.h" // This file contains our class "blueprint" we must include it so we can reference the class below
+
+using namespace std;
+
+
+int main() {
+
+  cout << "Let's Play Tic Tac Toe...\n";
+
+  // YOUR CODE GOES HERE
+
+}
