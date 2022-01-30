2e69da4f9efe8913df04482117f0569681932d11
profjeff <46285923+profjeff@users.noreply.github.com>
2022-01-30 09:53:50 -0800

    Create Tictactoe.cpp

---
+++ Tictactoe.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "Tictactoe.h" // The Tictactoe.h file contains our class "blueprint" we must include it so we can reference the class below
+
+/*
+
+  WRITE YOUR OUT-OF-CLASS constructors and methods HERE for the Tic Tac Toe class
+
+  Start each constructor with the name of your class followed by the scope resolution operator like ::, and then the class name (again)
+
+  Start each method with its return type, followed by the name of your class followed the scope resolution operator, and then the method name
+
+  Use the Tictactoe.h file as a guide (along with the README.md file) to help guide which constructors and methods you need to implement.
+
+*/
